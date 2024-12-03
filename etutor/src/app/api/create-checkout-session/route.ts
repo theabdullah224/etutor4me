0b1b02d181dfe661e71c324edae7fd447752a1e6
Abdullah <theabdullah224@gmail.com>
2024-12-03 15:34:03 +0500

    changes

--- etutor/src/app/api/create-checkout-session/route.ts
+++ etutor/src/app/api/create-checkout-session/route.ts
@@ -3,7 +3,7 @@ import Stripe from "stripe";
 import { PlanType, TutorLevel } from "../../../../types/Stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2023-10-16",
+  apiVersion: "2024-10-28.acacia",
 });
 
 // Mapping Tutor Levels and Plans to Stripe Price IDs
