3b69f2b960463640189fd113496af35eb5b0d87c
Abdullah <theabdullah224@gmail.com>
2024-12-10 16:04:57 +0500

    new fixes

--- etutor/src/app/parent/components/FindEtutor.tsx
+++ etutor/src/app/parent/components/FindEtutor.tsx
@@ -1,3 +1,6 @@
+"use client"
+
+
 import React, { useEffect, useState } from "react";
 import { ChevronLeft, ChevronRight, Calendar } from "lucide-react";
 import calendaricon from "../../../../public/calendaricongray.svg";
