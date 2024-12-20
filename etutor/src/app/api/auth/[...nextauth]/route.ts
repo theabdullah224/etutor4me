ae0d6ecca317d946d7e3bda1fdb3f3287c046227
Abdullah <theabdullah224@gmail.com>
2024-12-20 10:46:04 +0500

    fixes

--- etutor/src/app/api/auth/[...nextauth]/route.ts
+++ etutor/src/app/api/auth/[...nextauth]/route.ts
@@ -2,20 +2,11 @@
 
 
 
-import NextAuth, { NextAuthOptions } from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
-import { compare } from 'bcryptjs';
-import { connectMongoDB } from '../../connection/connection';
-import UserModel from '../../models/User';
-import jwt from 'jsonwebtoken';
-import { sendVerificationEmail } from '../../utils/sendEmail';
-import GoogleProvider from 'next-auth/providers/google';
+import NextAuth from 'next-auth';
+
 import {authOptions} from '@/app/auth/auth';
-interface Credentials {
-  role: string;
-  email: string;
-  password: string;
-}
+
+
 
 const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST, authOptions };
+export { handler as GET, handler as POST };
